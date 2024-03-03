77f1693dea034ee8652a32d126d458ce6e0d4fd2
Dan Schneider <dschneider8784@proton.me>
2024-03-03 11:37:14 -0500

    fix(bootstrap.ps1): change to function

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,20 +2,23 @@
 workflow New-ComputerSetup {
     # Install Winget
     # get latest download url
-    $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
-
-    if(!$hasPackageManager)
+    function InstallWinGet()
     {
-        Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-    
-        $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
-    
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        $releases = Invoke-RestMethod -uri $releases_url
-        $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
-    
-        "Installing winget from $($latestRelease.browser_download_url)"
-        Add-AppxPackage -Path $latestRelease.browser_download_url
+        $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
+
+        if(!$hasPackageManager)
+        {
+            Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+        
+            $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+        
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            $releases = Invoke-RestMethod -uri $releases_url
+            $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+        
+            "Installing winget from $($latestRelease.browser_download_url)"
+            Add-AppxPackage -Path $latestRelease.browser_download_url
+        }
     }
 
     # Install Nerd Fonts
